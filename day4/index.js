12dd457500b41374e1450db96c6a99e38186645f
berk <berk_baybas@hotmail.com>
2022-01-09 22:47:35 +0300

    day4 level2 exercise done

--- day4/index.js
+++ day4/index.js
@@ -49,3 +49,63 @@ if (number % 2 == 0) {
 } else {
   console.log(number + " is a odd number");
 }
+
+// *****************
+// Exercise: Level 2
+// *****************
+
+// 1)Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+const score = 80;
+
+if (score >= 80 && score <= 100) {
+  console.log("A");
+} else if (score >= 70 && score <= 89) {
+  console.log("B");
+} else if (score >= 60 && score <= 69) {
+  console.log("C");
+} else if (score >= 50 && score <= 59) {
+  console.log("D");
+} else if (score >= 0 && score <= 49) {
+  console.log("F");
+} else {
+  console.log("Invalid score");
+}
+
+// 2)Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+const month = "April";
+
+switch (month.toLowerCase()) {
+  case "september":
+  case "october":
+  case "november":
+    console.log("Season is Autumn");
+    break;
+  case "december":
+  case "January":
+  case "February":
+    console.log("Season is Winter");
+    break;
+  case "march":
+  case "april":
+  case "march":
+    console.log("Season is Spring");
+    break;
+  case "june":
+  case "july":
+  case "august":
+    console.log("Season is Summer");
+    break;
+  default:
+    console.log("invalid month");
+}
